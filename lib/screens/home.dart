a8b91d751df3843ff51571b87316f1fa55a68a18
Ezekiel ADEJOBI <ezeko2017@gmail.com>
2020-09-09 18:01:22 +0100

    add form fields

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -10,8 +8,8 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    var deviceSize = MediaQuery.of(context).size.height / 2;
-    print(deviceSize);
+    //var deviceSize = MediaQuery.of(context).size.height / 2;
+    //print(deviceSize);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue[700],
@@ -41,42 +39,85 @@ class _HomeState extends State<Home> {
               context: context,
               backgroundColor: Colors.transparent,
               builder: (context) => Card(
-                    child: Center(
-                      child: Scaffold(
-                          body: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          mainAxisSize: MainAxisSize.max,
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: <Widget>[
-                            SizedBox(
-                              height: 10.0,
-                            ),
-                            Text(
-                              'Add Contact Here',
-                              style: TextStyle(
-                                fontSize: 18.0,
-                                color: Colors.blueGrey,
-                              ),
-                            ),
-                            SizedBox(
-                              height: 10.0,
-                            ),
-                            TextField(
-                              textAlignVertical: TextAlignVertical.top,
-                              obscureText: false,
-                              decoration: InputDecoration(
-                                border: OutlineInputBorder(),
-                                labelText: 'Name',
+                    child: Scaffold(
+                        body: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Expanded(
+                                                      child: Column(
+                              mainAxisSize: MainAxisSize.max,
+                              //crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: <Widget>[
+                                SizedBox(
+                                  height: 10.0,
+                                ),
+                                Text(
+                                  'Add Contact Here',
+                                  style: TextStyle(
+                                    fontSize: 18.0,
+                                    color: Colors.blueGrey,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 10.0,
+                                ),
+                                Form(
+                                  child: Column(
+                                    children: <Widget>[
+                                      TextFormField(
+                                          autocorrect: true,
+                                          autofocus: true,
+                                          textAlignVertical: TextAlignVertical.top,
+                                          obscureText: false,
+                                          enableSuggestions: true,
+                                          keyboardType: TextInputType.name,
+                                          autofillHints: null,
+                                          textCapitalization: TextCapitalization.sentences,
+                                          decoration: InputDecoration(
+                                            border: OutlineInputBorder(),
+                                            labelText: 'Name',
 
-                              ),
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          height: 10.0,
+                                        ),
+                                        TextFormField(
+                                          textAlignVertical: TextAlignVertical.top,
+                                          obscureText: false,
+                                          autofillHints: null,
+                                          keyboardType: TextInputType.streetAddress,
+                                          decoration: InputDecoration(
+                                            border: OutlineInputBorder(),
+                                            labelText: 'Address',
+
+                                          ),
+                                        ),
+
+                                        SizedBox(
+                                          height: 10.0,
+                                        ),
+
+                                        TextFormField(
+                                          textAlignVertical: TextAlignVertical.top,
+                                          keyboardType: TextInputType.phone,
+                                          autocorrect: true,
+                                          autofillHints: null,
+                                          maxLengthEnforced: true,
+                                          maxLength: 13,
+                                          decoration: InputDecoration(
+                                            border: OutlineInputBorder(),
+                                            labelText: '070123456789'
+                                          ),
+                                        )
+                              ]
                             )
-                          ],
+                                  
+                              ),
+                              ],
+                            ),
+                          ),
                         ),
 
-                      ),
-
-                      ),
                     ),
                   ),
 
